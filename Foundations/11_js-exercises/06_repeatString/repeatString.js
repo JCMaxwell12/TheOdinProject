657004b81cbb1fbb77194139054e361d3d68c0a5
JCMaxwell12 <69727413+JCMaxwell12@users.noreply.github.com>
2025-05-08 16:30:09 -0600

    Add Js exercise 6

--- Foundations/11_js-exercises/06_repeatString/repeatString.js
+++ Foundations/11_js-exercises/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+  let repeatedString = '';
+  if (num < 0) {
+    return 'ERROR';
+  }
 
-};
+  for (let iterationNum = 0; iterationNum < num; iterationNum++) {
+    for (let charIndex = 0; charIndex < word.length; charIndex++) {
+      repeatedString = repeatedString + word.charAt(charIndex);
+    }
+  }
+  return repeatedString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
