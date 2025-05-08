821a1079ca52e27ed794b7ed089ec20d42a0c2ec
JCMaxwell12 <69727413+JCMaxwell12@users.noreply.github.com>
2025-05-08 16:42:16 -0600

    Add Js exercise 7

--- Foundations/11_js-exercises/07_reverseString/reverseString.js
+++ Foundations/11_js-exercises/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  let reversedString = '';
+  for (let charIndex = word.length; charIndex >= 0; charIndex--) {
+    reversedString = reversedString + word.charAt(charIndex);
+  }
 
+  return reversedString
 };
 
 // Do not edit below this line
