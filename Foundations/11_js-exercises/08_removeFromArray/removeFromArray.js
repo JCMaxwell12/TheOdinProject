d9fb566f236e658fa62f873b75ee56ed5bd4c7fa
JCMaxwell12 <69727413+JCMaxwell12@users.noreply.github.com>
2025-05-08 17:26:56 -0600

    Add Js exercise 8

--- Foundations/11_js-exercises/08_removeFromArray/removeFromArray.js
+++ Foundations/11_js-exercises/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(ary, ...args) {
+  for (let argIndex = 0; argIndex < args.length; argIndex++) {
+    for (let aryArg = 0; aryArg < ary.length; aryArg++) {
+      if (ary[aryArg] === args[argIndex]) {
+        ary.splice(aryArg, 1);
+        aryArg--; // Prevent loop from skipping an argument
+      }
+    }
+  }
+
+  return ary
 };
 
 // Do not edit below this line
