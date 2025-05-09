5a489e47c95a748625753214afad2c3dda9ffd18
JCMaxwell12 <69727413+JCMaxwell12@users.noreply.github.com>
2025-05-08 18:15:15 -0600

    Add Js exercise 9

--- Foundations/11_js-exercises/09_sumAll/sumAll.js
+++ Foundations/11_js-exercises/09_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+  let digits = [];
 
+  if (typeof(startInt) != 'number' ||
+    typeof(endInt) != 'number' ||
+    !Number.isInteger(endInt) ||
+    !Number.isInteger(startInt) ||
+    startInt < 0 ||
+    startInt < 0
+  ){
+    return 'ERROR'
+  }
+
+  if (startInt < endInt) {
+    for (let  digit = startInt; digit <= endInt; digit++) {
+      digits.push(digit);
+    }
+  }
+  else {
+    for (let  digit = startInt; digit >= endInt; digit--) {
+      digits.push(digit);
+    }
+  }
+  let sum = 0;
+  for (let index = 0; index < digits.length; index++) {
+    sum = sum + digits[index];
+  }
+  return sum
 };
 
 // Do not edit below this line
