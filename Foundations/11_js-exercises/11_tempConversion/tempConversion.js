050ec7bfcbe1f3a71ef3e9a35c8ac0eea356b995
JCMaxwell12 <69727413+JCMaxwell12@users.noreply.github.com>
2025-05-11 19:25:15 -0600

    Add Js exercise 11

--- Foundations/11_js-exercises/11_tempConversion/tempConversion.js
+++ Foundations/11_js-exercises/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = ((fahrenheit - 32) * (5/9));
+  let celsiusRounded = parseFloat(celsius.toFixed(1));
+  return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * (9/5) + 32);
+  let fahrenheitRounded = parseFloat(fahrenheit.toFixed(1));
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
