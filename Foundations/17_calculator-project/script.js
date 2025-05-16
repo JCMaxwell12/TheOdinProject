38996adb3086dca158a1a3a6bf7871628d466a79
JCMaxwell12 <69727413+JCMaxwell12@users.noreply.github.com>
2025-05-16 15:19:13 -0600

    Add operate() func

--- Foundations/17_calculator-project/script.js
+++ Foundations/17_calculator-project/script.js
@@ -5,3 +5,12 @@ const sub = (a, b) => a - b;
 const mul = (a, b) => a * b;
 
 const div = (a, b) => a / b;
+
+const operate = function (operator, a, b) {
+	switch (operator) {
+		case '+': return add(a,b);
+		case '-': return sub(a,b);
+		case '*': return mul(a,b);
+		case '/': return div(a,b);
+	}
+}
