1c35fa3e8a455bd16672241d3667232901b21e1f
JCMaxwell12 <69727413+JCMaxwell12@users.noreply.github.com>
2025-05-15 19:41:40 -0600

    Add Js exercise 14

--- Foundations/11_js-exercises/14_fibonacci/fibonacci.js
+++ Foundations/11_js-exercises/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+	if (num < 0) return "OOPS";
 
+	let ary = [0, 1, 1];
+	if (num <= 2) return ary[num];
+
+	num = num - 2;
+	while (num > 0) {
+		ary.push(ary[1] + ary[2]);
+		ary.shift(1);
+		num--;
+	}
+	return ary[2];
 };
 
 // Do not edit below this line
