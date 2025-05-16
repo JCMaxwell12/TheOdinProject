5abfb62dbc9666ef50290b51520d4d102f12500b
JCMaxwell12 <69727413+JCMaxwell12@users.noreply.github.com>
2025-05-16 14:36:41 -0600

    Add Js exercise 16

--- Foundations/11_js-exercises/16_findTheOldest/findTheOldest.js
+++ Foundations/11_js-exercises/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+let findTheOldest = function(people) {
+	const getAge = function(person) {
+		if (!person.yearOfDeath) {
+			return new Date().getFullYear() - person.yearOfBirth;
+		}
 
+		return person.yearOfDeath - person.yearOfBirth;
+	}
+	const oldestPerson = people.reduce(
+		function (oldest, cVal) {
+			if (getAge(oldest) < getAge(cVal)) {
+				oldest = cVal;
+			}
+			return oldest;
+	})
+	return oldestPerson;
 };
 
 // Do not edit below this line
