e7251ce7091fa25b245cf201e004c79d55fe6cba
JCMaxwell12 <69727413+JCMaxwell12@users.noreply.github.com>
2025-05-15 19:13:00 -0600

    Add Js exercise 13

--- Foundations/11_js-exercises/13_palindromes/palindromes.js
+++ Foundations/11_js-exercises/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const reverseStr = function (text) {
+	return text.split("").reverse().join("");
+}
 
+const palindromes = function (text) {
+	text = text.replace(/[^\w\']/g, "");
+	text = text.toLowerCase();
+	if (reverseStr(text) == text) return true;
+	return false;
 };
 
 // Do not edit below this line
