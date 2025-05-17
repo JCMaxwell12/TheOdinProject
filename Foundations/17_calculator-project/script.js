201ddf2507eae1055a28578e6677bf1633c25918
JCMaxwell12 <69727413+JCMaxwell12@users.noreply.github.com>
2025-05-16 20:18:06 -0600

    Add display and clear funcs

--- Foundations/17_calculator-project/script.js
+++ Foundations/17_calculator-project/script.js
@@ -15,6 +15,40 @@ const operate = function (operator, a, b) {
 	}
 }
 
+const round = function(num, digits) {
+	let numStr = String(num);
+	let dotPosn = numStr.indexOf('.');
+	
+	if (numStr.length <= digits) return numStr;
+	
+	if (dotPosn > digits) return 'OL';
+
+	if (dotPosn == digits) return round(num);
+	
+	let precision = digits - dotPosn - 1; 	// Get digits after the . to keep
+	let newNum = Number(num.toFixed(precision));
+
+	return newNum;
+}
+
+const display = function(num, digits) {
+	let val = ''
+	if (typeof(num) == 'number') val = round(num, digits);
+	if (typeof(num) == 'string') val = num;
+	
+	screen.textContent = val;
+}
+
+const clear = function() {
+	num1 = 0;
+	num2 = 0;
+	operator = '';
+	display()
+}
+
+
 let num1 = 0;
 let num2 = 0;
 let operator = '';
+
+const screen = document.querySelector('.screen');
